3edcf83e30f9ae1920c2cc90ea9dfc388b616b9a
MVK <2300031063@kluniversity.in>
2025-08-27 14:09:08 +0530

    React Project V1.0

---
+++ BOOKAPI-REACT/src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+import BookManager from "./components/BookManager"
+
+function App() {
+
+  return (
+    <>
+      <BookManager/>
+    </>
+  )
+}
+
+export default App
